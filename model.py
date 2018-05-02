d13678e09db79ab282e9a0e025777420dc402b17
tc-imba <liuyh615@sjtu.edu.cn>
2018-05-03 02:06:59 +0800

    enforce_stationarity=False

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ import statsmodels.api as sm
 def analyze(data, time, predict_start, predict_end, disp=False):
     d = pandas.Series(data)
     d.index = pandas.Index(sm.tsa.datetools.dates_from_str(time))
-    d = d.asfreq('H', method='backfill')
-    mod = sm.tsa.statespace.SARIMAX(d, trend='n', order=(0, 1, 0), seasonal_order=(1, 1, 1, 24)).fit(disp=disp)
+    d = d.asfreq('H', method='pad')
+    mod = sm.tsa.statespace.SARIMAX(d, trend='n', order=(0, 1, 0), seasonal_order=(1, 1, 1, 24),
+                                    enforce_stationarity=False).fit(disp=disp)
     predict = mod.predict(predict_start, predict_end, dynamic=True)
     if disp:
         fig, ax = plt.subplots(figsize=(12, 8))
@@ -15,4 +16,3 @@ def analyze(data, time, predict_start, predict_end, disp=False):
         predict.plot(ax=ax)
         fig.show()
     return predict
-
